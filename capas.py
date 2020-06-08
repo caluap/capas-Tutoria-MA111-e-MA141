b301ad463103cbc910302601e909e37374feb3fa
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-06-08 17:19:36 -0300

    lighter shade of pale

--- capas.py
+++ capas.py
@@ -3,7 +3,7 @@ size("A5")
 margins = (12.34, 13.23)
 size = (width()-(margins[0]*2), height() - (margins[1]*2))
 
-bg_color = (.18,.16,.24,0)
+bg_color = (.09,.1,.16,0)
 main_color = (0,.44,1,0)
     
 cmykFill(*bg_color)
