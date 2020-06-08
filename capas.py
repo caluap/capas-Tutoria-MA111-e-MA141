b2f33ca17a7b52f189d204a2782ca7b8970a332c
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-06-08 17:19:20 -0300

    content structure

--- capas.py
+++ capas.py
@@ -8,3 +8,16 @@ main_color = (0,.44,1,0)
     
 cmykFill(*bg_color)
 rect(margins[0], margins[1], size[0], size[1])
+
+
+# 1. Introdução
+# 2. Matrizes 2 × 2
+# 3. Potências, equações exponenciais e logaritmos
+# 4. Polinômios e afins
+# 5. Trigonometria e Vetores
+# 6. Troca de variáveis e composição de funções
+# 7. Equação de retas e circunferências
+# 8. Transformações em gráficos
+# 9. Revisão para integrais por partes
+# 10. Revisão para integrais trigonométricas
+# 11. Revisão para frações parciais
