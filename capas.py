9078b7ae9931c16cddf64d836cffdb908863b9f6
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-06-08 22:58:10 -0300

    proposal for matrizes 2x2

--- capas.py
+++ capas.py
@@ -2,16 +2,46 @@ size("A5")
 
 margins = (12.34, 13.23)
 size = (width()-(margins[0]*2), height() - (margins[1]*2))
+w = size[0]
+h = size[1]
 
 bg_color = (.09,.1,.16,0)
 main_color = (0,.44,1,0)
     
-cmykFill(*bg_color)
-rect(margins[0], margins[1], size[0], size[1])
-
-
 # 1. Introdução
+
 # 2. Matrizes 2 × 2
+
+translate(margins[0], margins[1])
+cmykFill(*bg_color)
+#rect(0,0, w, h)
+
+cmykFill(*main_color)
+
+grid_gap = 1.5
+cols = 44
+
+mod_size_x = (w - (grid_gap * (cols-1))) / cols
+
+ratio = h/w
+rows = round(ratio*cols)
+
+angle_y = 45 / (rows-1)
+angle_x = 45 / (cols-1)
+
+mod_size_y = (h - (grid_gap * (rows -1))) / rows
+
+for x in range(cols):
+    for y in range(rows):
+        with savedState():
+            xc = x*(mod_size_x+grid_gap) + mod_size_x/2
+            xy = y*(mod_size_y+grid_gap) + mod_size_y/2
+            angle = angle_y*y + angle_x*x
+            rotate(angle, center=(xc,xy))
+            #skew(angle_s*x,center=(xc,xy))
+            rect(x*(mod_size_x+grid_gap),y*(mod_size_y+grid_gap), mod_size_x, mod_size_y)
+
+
 # 3. Potências, equações exponenciais e logaritmos
 # 4. Polinômios e afins
 # 5. Trigonometria e Vetores
